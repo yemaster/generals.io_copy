d6a441967f8790385b8f9b05260c4c7efd3132cc
Yemaster <30290923+yemaster@users.noreply.github.com>
2021-08-12 21:11:49 +0800

    Update new.js

--- bot/new.js
+++ bot/new.js
@@ -51,8 +51,10 @@ socket.on('connect', function () {
         'nickname': '[bot]' + genName()
     })
     socket.emit('send_message', { text: '我来了', team: 0 })
-    socket.emit('change_ready', { ready: 1 })
-    socket.emit('send_message', { text: '开始吧', team: 0 })
+    setTimeout(function() {
+        socket.emit('change_ready', { ready: 1 })
+        socket.emit('send_message', { text: '开始吧', team: 0 })
+    }, 5000)
     socket.emit('change_game_conf', {
         height_ratio: "1",
         width_ratio: "1",
@@ -70,7 +72,7 @@ socket.on('starting', function (data) {
     socket.emit('send_message', { text: '总算开始了，我等的花儿都谢了', team: 0 })
 });
 
-let grid_type, army_cnt
+let grid_type, army_cnt, is_in
 let n, m;
 let generalXY
 let client_id
@@ -86,6 +88,9 @@ socket.on('init_map', function (data) {
     grid_type = Array(n)
     for (let i = 0; i < n; ++i)
         grid_type[i] = Array(m)
+    is_in = Array(n)
+    for (let i = 0; i < n; ++i)
+        is_in[i] = Array(m)
     army_cnt = Array(n)
     for (let i = 0; i < n; ++i)
         army_cnt[i] = Array(m)
@@ -162,15 +167,15 @@ function FindNearestEnemy() {
 }
 
 function ArmiesAround() {
-    let startX = generalXY[0] - 5
-    let startY = generalXY[1] - 5
+    let startX = generalXY[0] - 3
+    let startY = generalXY[1] - 3
     if (startX < 0)
         startX = 0
     if (startY < 0)
         startY = 0
     let Count = 0
-    for (let i = startX; (i < n) && (i <= generalXY[0] + 5); ++i) {
-        for (let j = startY; (j < m) && (j <= generalXY[1] + 5); ++j)
+    for (let i = startX; (i < n) && (i <= generalXY[0] + 3); ++i) {
+        for (let j = startY; (j < m) && (j <= generalXY[1] + 3); ++j)
             if (grid_type[i][j] < 100 && grid_type[i][j] % 50 != player && grid_type[i][j] != 50) {
                 Count += army_cnt[i][j]
             }
@@ -220,18 +225,66 @@ function gatherArmyToMy(x, y, need) {
 }
 
 let moveOrders = []
+let weight = []
 
-function goFuck(x, y, need) {
+function goFuck(x, y, startx, starty, last, need) {
     isDB = true
     if (army_cnt[x][y] == 1)
         return
     Vis[x * m + y] = true
     let dx = [-1, 0, 1, 0]
     let dy = [0, -1, 0, 1]
+    let k = [0, 0, 0, 0]
+    let backLast = [2, 3, 0, 1]
+    for (let i = 0; i < 4; ++i)
+        k[i] += weight[i]
+    if (last >= 0 && last <= 3)
+        k[last] = 10, k[backLast[last]] -= 10
+    let sx = [0, 1, 2, 3]
     let hlf = false
+    for (let i = x - 1; i <= x + 1; ++i) {
+        for (let j = y - 1; j <= y + 1; ++j) {
+            if (i < 0)
+                continue
+            if (j < 0)
+                continue
+            if (i >= n)
+                continue
+            if (j >= m)
+                continue
+            if (i == x - 1 && grid_type[i][j] < 150 && grid_type[i][j] % 50 == 0)
+                k[0] += army_cnt[i][j]
+            if (i == x + 1 && grid_type[i][j] < 150 && grid_type[i][j] % 50 == 0)
+                k[2] += army_cnt[i][j]
+            if (j == y - 1 && grid_type[i][j] < 150 && grid_type[i][j] % 50 == 0)
+                k[1] += army_cnt[i][j]
+            if (j == y + 1 && grid_type[i][j] < 150 && grid_type[i][j] % 50 == 0)
+                k[3] += army_cnt[i][j]
+            if (grid_type[i][j] >= 50 && grid_type[i][j] < 100) {
+                console.log(`Got City at (${i},${j})`)
+                if (grid_type[i][j] % 50 != player && is_in[i][j] != true) {
+                    CityQueue.push({ x: i, y: j })
+                    is_in[i][j] = true
+                }
+            }
+            if (grid_type[i][j] >= 100 && grid_type[i][j] < 150) {
+                console.log(`Got General at (${i},${j})`)
+                if (grid_type[i][j] % 50 != player && is_in[i][j] != true) {
+                    GeneralQueue.push({ x: i, y: j })
+                    is_in[i][j] = true
+                }
+            }
+        }
+    }
+    sx.sort(function (a, b) {
+        if (k[b] == k[a])
+            return Math.random() - 0.5;
+        return Math.random() - k[a] / (k[b] + k[a]);
+    })
+    weight[sx[0]] += Math.floor(k[sx[0]] / 10)
     for (let i = 0; i < 4; ++i) {
-        let gox = x + dx[i]
-        let goy = y + dy[i]
+        let gox = x + dx[sx[i]]
+        let goy = y + dy[sx[i]]
         if (gox >= n)
             continue
         if (goy >= m)
@@ -242,20 +295,17 @@ function goFuck(x, y, need) {
             continue
         if (Vis[gox * m + goy] == true)
             continue
-        if (Math.abs(generalXY[0] - x) + Math.abs(generalXY[1] - y) > Math.abs(generalXY[0] - gox) + Math.abs(generalXY[1] - goy)) {
+        if (Math.abs(startx - x) + Math.abs(starty - y) + 6 > Math.abs(startx - gox) + Math.abs(starty - goy)) {
             continue
         }
         if (grid_type[gox][goy] < 150 && grid_type[gox][goy] % 50 != player &&
-                army_cnt[gox][goy] > army_cnt[x][y])
+            army_cnt[gox][goy] > army_cnt[x][y])
             continue
         if (grid_type[gox][goy] != 201) {
-            if (grid_type[gox][goy] >= 50 && grid_type[gox][goy] < 100 && grid_type[gox][goy] % 50 != player)
-                CityQueue.push({ x: gox, y: goy })
-            if (grid_type[gox][goy] >= 100 && grid_type[gox][goy] < 150 && grid_type[gox][goy] % 50 != player)
-                GeneralQueue.push({ x: gox, y: goy })
             socket.emit('attack', { x: x, y: y, dx: gox, dy: goy, half: need })
             moveOrders.push({ x: gox, y: goy, dx: x, dy: y, half: need })
-            goFuck(gox, goy, false)
+            goFuck(gox, goy, startx, starty, last, false)
+            return
         }
     }
 }
@@ -291,7 +341,7 @@ function Conquer(x, y, need) {
             vis[(cur.x + 1) * m + cur.y] = true
             q.push({ x: cur.x + 1, y: cur.y })
             if (grid_type[cur.x + 1][cur.y] % 50 == player && grid_type[cur.x + 1][cur.y] < 150)
-            Army_cnt += army_cnt[cur.x + 1][cur.y] - 1
+                Army_cnt += army_cnt[cur.x + 1][cur.y] - 1
             move_order.push({ x: cur.x + 1, y: cur.y, dx: cur.x, dy: cur.y, half: false })
         }
         if (cur.y + 1 < m && (!vis[cur.x * m + cur.y + 1]) && army_cnt[cur.x][cur.y + 1] > 0 && (grid_type[cur.x][cur.y + 1] < 150 || grid_type[cur.x][cur.y + 1] == 200 || grid_type[cur.x][cur.y + 1] == 202)) {
@@ -302,9 +352,9 @@ function Conquer(x, y, need) {
             move_order.push({ x: cur.x, y: cur.y + 1, dx: cur.x, dy: cur.y, half: false })
         }
     }
-    if (Army_cnt >= need) {
+    //if (Army_cnt >= need) {
         doMove(move_order)
-    }
+    //}
 }
 let dfs_path = []
 
@@ -317,11 +367,37 @@ function explore(x, y, i) {
     isDB = true
     if (army_cnt[x][y] == 1)
         return
+    for (let i = x - 1; i <= x + 1; ++i) {
+        for (let j = y - 1; j <= y + 1; ++j) {
+            if (i < 0)
+                continue
+            if (j < 0)
+                continue
+            if (i >= n)
+                continue
+            if (j >= m)
+                continue
+            if (grid_type[i][j] >= 50 && grid_type[i][j] < 100) {
+                console.log(`Got City at (${i},${j})`)
+                if (grid_type[i][j] % 50 != player && is_in[i][j] != true) {
+                    CityQueue.push({ x: i, y: j })
+                    is_in[i][j] = true
+                }
+            }
+            if (grid_type[i][j] >= 100 && grid_type[i][j] < 150) {
+                console.log(`Got General at (${i},${j})`)
+                if (grid_type[i][j] % 50 != player && is_in[i][j] != true) {
+                    GeneralQueue.push({ x: i, y: j })
+                    is_in[i][j] = true
+                }
+            }
+        }
+    }
     Vis[x * m + y] = true
     let dx = [-1, 0, 1, 0]
     let dy = [0, -1, 0, 1]
     let hlf = false
-    if (army_cnt[x][y] > 500)
+    if (army_cnt[x][y] > 10)
         hlf = true
     i = Math.floor(Math.random() * 4)
     let gox = x + dx[i]
@@ -341,15 +417,18 @@ function explore(x, y, i) {
         socket.emit('attack', { x: x, y: y, dx: gox, dy: goy, half: hlf })
         explore(gox, goy, i)
     }
-    if (grid_type[gox][goy] >= 50 && grid_type[gox][goy] < 100) {
+    if (grid_type[gox][goy] >= 50 && grid_type[gox][goy] < 100 && is_in[gox][goy] != true) {
         console.log(`Got City at (${gox},${goy})`)
         CityQueue.push({ x: gox, y: goy })
+        is_in[gox][goy] = true
     }
     if (grid_type[gox][goy] >= 100 && grid_type[gox][goy] < 150) {
         console.log(`Got General at (${gox},${goy})`)
         socket.emit('attack', { x: x, y: y, dx: gox, dy: goy, half: false })
-        if (grid_type[gox][goy] % 50 != player)
+        if (grid_type[gox][goy] % 50 != player && is_in[gox][goy] != true) {
             GeneralQueue.push({ x: gox, y: goy })
+            is_in[gox][goy] = true
+        }
     }
     if (grid_type[gox][goy] < 50) {
         socket.emit('attack', { x: x, y: y, dx: gox, dy: goy, half: hlf })
@@ -358,8 +437,10 @@ function explore(x, y, i) {
 }
 let head = 0
 
+
+// 更新
 socket.on('update', function (data) {
-    if (data.is_diff) {
+    if (data.is_diff) {   //更新
         for (let i = 0; i * 2 < data.grid_type.length; ++i) {
             let t = data.grid_type[i * 2];
             grid_type[parseInt(t / m)][t % m] = data.grid_type[i * 2 + 1]
@@ -381,9 +462,27 @@ socket.on('update', function (data) {
             }
         }
     }
+    for (let i = 0; i < n; ++i)
+        for (let j = 0; j < m; ++j) {
+            if (grid_type[i][j] >= 50 && grid_type[i][j] < 100) {
+                console.log(`Got City at (${i},${j})`)
+                if (grid_type[i][j] % 50 != player && is_in[i][j] != true) {
+                    CityQueue.push({ x: i, y: j })
+                    is_in[i][j] = true
+                }
+            }
+            if (grid_type[i][j] >= 100 && grid_type[i][j] < 150) {
+                console.log(`Got General at (${i},${j})`)
+                if (grid_type[i][j] % 50 != player && is_in[i][j] != true) {
+                    GeneralQueue.push({ x: i, y: j })
+                    is_in[i][j] = true
+                }
+            }
+        }
     let MyCount = Total()
     console.log("MyCount:", MyCount)
     if (MyCount.Land == 0) {
+        process.exit(0)
         return
     }
     let TurnTot = data.turn
@@ -393,16 +492,36 @@ socket.on('update', function (data) {
     console.log("Nearest Enemy:" + NearestEnemy)
     let CountArmyAround = ArmiesAround()
     console.log(`Found ${CountArmyAround}  Enemies Around`)
-    if (CountArmyAround > 80) {
+    for (let i = 0; i < GeneralQueue.length; ++i) {
+        if (grid_type[GeneralQueue[i].x][GeneralQueue[i].y] % 50 != player && grid_type[GeneralQueue[i].x][GeneralQueue[i].y] >= 100 && grid_type[GeneralQueue[i].x][GeneralQueue[i].y] < 150) {
+            isFind = true
+            /*socket.emit('send_message', {
+                text: `${GeneralQueue[i].x} ${GeneralQueue[i].y} 你家要没了`,
+                team: 0
+            })*/
+            Conquer(GeneralQueue[i].x, GeneralQueue[i].y, 19190810)
+            setTimeout(function () { isFind = false }, 100)
+            return
+        }
+    }
+    if (CountArmyAround > generalXY) {
         console.log("Save My Home")
         let need = CountArmyAround + 3 - army_cnt[generalXY[0]][generalXY[1]]
         gatherArmyToMy(generalXY[0], generalXY[1], Math.floor(MyCount.Army / 2))
         console.log("Do Fuck")
         isFind = true
-        goFuck(generalXY[0], generalXY[1], true)
+        //gatherArmyToMy(generalXY[0], generalXY[1], 8798979)
+        let goFuckSelect = []
+        for (let i = 0; i < n; ++i)
+            for (let j = 0; j < m; ++j)
+                if (grid_type[i][j] < 150 && grid_type[i][j] % 50 == player && army_cnt[i][j] > 20 && i != generalXY[0] && j != generalXY[1]) {
+                    goFuckSelect.push({ x: i, y: j })
+                }
+        for (let i = 0; i < goFuckSelect.length; ++i)
+            goFuck(goFuckSelect[i].x, goFuckSelect[i].y, goFuckSelect[i].x, goFuckSelect[i].y, 114514, false)
         for (let i = moveOrders.length - 1; i >= 0; --i)
             socket.emit('attack', moveOrders[i])
-        isFind = false
+        setTimeout(function () { isFind = false }, 100)
     }
     else {
         if (isFind == true)
@@ -411,90 +530,41 @@ socket.on('update', function (data) {
         Vis = Array(n * m)
         dfs_path = []
         ++head;
-        if (head % 10 < 6) {
-            if (MyCount.Army > 200) {
-                head = 2
-                return
-            }
+        if (MyCount.Army > 150 && head % 20 == 0) {
+            console.log("Do Fuck")
+            isFind = true
+            //gatherArmyToMy(generalXY[0], generalXY[1], 8798979)
+            let goFuckSelect = []
+            for (let i = 0; i < n; ++i)
+                for (let j = 0; j < m; ++j)
+                    if (grid_type[i][j] < 150 && grid_type[i][j] % 50 == player && army_cnt[i][j] > 20) {
+                        goFuckSelect.push({ x: i, y: j })
+                    }
+            for (let i = 0; i < goFuckSelect.length; ++i)
+                goFuck(goFuckSelect[i].x, goFuckSelect[i].y, goFuckSelect[i].x, goFuckSelect[i].y, 114514, false)
+            for (let i = moveOrders.length - 1; i >= 0; --i)
+                socket.emit('attack', moveOrders[i])
+            setTimeout(function () { isFind = false }, 100)
+            return
+        }
+        else {
             console.log("Do Normal Explore")
             isFind = true
             for (let i = 0; i < n; ++i)
                 for (let j = 0; j < m; ++j)
                     if (grid_type[i][j] % 50 == player && army_cnt[i][j] > 0)
                         explore(i, j, Math.floor(Math.random() * 4))
-            isFind = false
+            setTimeout(function () { isFind = false }, 100)
         }
-        else if (head % 10 < 7) {
-            let k = []
-            for (let i = 0; i < n; ++i)
-                for (let j = 0; j < m; ++j)
-                    if (grid_type[i][j] % 50 == player && army_cnt[i][j] > 10)
-                        k.push({x:i, y:j})
-            if (k.length < 1)
-                return
-            isFind = true
-            let z = k[Math.floor(Math.random() * k.length)]
-            let fx = Math.floor(Math.random() * 4)
-            let dx = [-1, 0, 1, 0]
-            let dy = [0, -1, 0, 1]
-            let depth = 0
-            while (true) {
-                let gox = z.x + dx[fx]
-                let goy = z.y + dy[fx]
-                if (gox < 0)
-                    break
-                if (goy < 0)
-                    break
-                if (gox >= n)
-                    break
-                if (goy >= m)
-                    break
-                console.log(gox, goy)
-                ++depth
-                if (depth > 100)
-                    break
-                if (army_cnt[z.x][z.y] <= 1)
-                    break
-                if (grid_type[gox][goy] >= 201)
-                    break
-                if (grid_type[gox][goy] >= 100 && grid_type[gox][goy] < 150 && grid_type[gox][goy] % 50 != player)
-                    GeneralQueue.push({ x: gox, y: goy })
+        moveOrders = []
+        for (let i = 0; i < CityQueue.length; ++i) {
+            if (grid_type[CityQueue[i].x][CityQueue[i].y] % 50 != player && ((army_cnt[CityQueue[i].x][CityQueue[i].y] > 0 && MyCount.Army > army_cnt[CityQueue[i].x][CityQueue[i].y] + 20) || MyCount.Army > 80)) {
+                isFind = true
+                Conquer(CityQueue[i].x, CityQueue[i].y, army_cnt[CityQueue[i].x][CityQueue[i].y] > 0 ? army_cnt[CityQueue[i].x][CityQueue[i].y] + 20 : 100)
 
-                if (grid_type[gox][goy] >= 50 && grid_type[gox][goy] < 100 && grid_type[gox][goy] % 50 != player)
-                    CityQueue.push({ x: gox, y: goy })
-                z.x = gox
-                z.y = goy
-            }
-            isFind = false
-        }
-        else if (head % 10 < 9) {
-            if (MyCount.Army - MyCount.Land < 40)
+                setTimeout(function() { isFind = false} , 100)
                 return
-            console.log("Do Fuck")
-            isFind = true
-            gatherArmyToMy(generalXY[0], generalXY[1], 8798979)
-            goFuck(generalXY[0], generalXY[1], 0)
-            for (let i = moveOrders.length - 1; i >= 0; --i)
-                socket.emit('attack', moveOrders[i])
-            isFind = false
+            }
         }
-            moveOrders = []
-            for (let i = 0; i < GeneralQueue.length; ++i) {
-                if (grid_type[GeneralQueue[i].x][GeneralQueue[i].y] % 50 != player) {
-                    isFind = true
-                    Conquer(GeneralQueue[i][i].x, GeneralQueue[i][i].y, 19190810)
-                    isFind = false
-                    return
-                }
-            }
-            for (let i = 0; i < CityQueue.length; ++i) {
-                if (grid_type[CityQueue[i].x][CityQueue[i].y] % 50 != player && ((army_cnt[CityQueue[i].x][CityQueue[i].y] > 0 && MyCount.Army > army_cnt[CityQueue[i].x][CityQueue[i].y] + 20) || MyCount.Army > 80)) {
-                    isFind = true
-                    Conquer(CityQueue[i].x, CityQueue[i].y, army_cnt[CityQueue[i].x][CityQueue[i].y] > 0 ? army_cnt[CityQueue[i].x][CityQueue[i].y] + 20 : 100)
-
-                    isFind = false
-                    return
-                }
-            }
     }
 });
